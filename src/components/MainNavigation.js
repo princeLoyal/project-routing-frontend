ef3ffb78f00e02a3a9f85b4ea0367b235ea92d58
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-13 21:52:48 +0100

    Update MainNavigation.js

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,6 +1,48 @@
-function MainNavigation () {
-return (
-   <div>Hello Main</div>
-);
+import { NavLink } from 'react-router-dom';
+
+import classes from './MainNavigation.module.css';
+//import NewsletterSignup from './NewsletterSignup';
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/events"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Events
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/newsletter"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Newsletter
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
+
 export default MainNavigation;
