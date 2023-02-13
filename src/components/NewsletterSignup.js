918eff5b494d28b7b0f2fab729c043b81d1c4542
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-13 21:57:36 +0100

    Update NewsletterSignup.js

--- src/components/NewsletterSignup.js
+++ src/components/NewsletterSignup.js
@@ -1,49 +1,32 @@
-import { NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useFetcher } from 'react-router-dom';
 
-import classes from './MainNavigation.module.css';
-import NewsletterSignup from './NewsletterSignup';
+import classes from './NewsletterSignup.module.css';
 
-function MainNavigation() {
+function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const { data, state } = fetcher;
+
+  useEffect(() => {
+    if (state === 'idle' && data && data.message) {
+      window.alert(data.message);
+    }
+  }, [data, state]);
+
   return (
-    <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              end
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/events"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Events
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/newsletter"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Newsletter
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <NewsletterSignup />
-    </header>
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
+      <input
+        type="email"
+        placeholder="Sign up for newsletter..."
+        aria-label="Sign up for newsletter"
+      />
+      <button>Sign up</button>
+    </fetcher.Form>
   );
 }
 
-export default MainNavigation;
+export default NewsletterSignup;
