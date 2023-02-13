efa4ad68d1969c21b42834609cb284229624a71f
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-14 00:41:11 +0100

    Update EventDetail.js

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -57,21 +57,3 @@ export async function loader({ request, params }) {
     events: loadEvents(),
   });
 }
-
-export async function action({ params, request }) {
-  const eventId = params.eventId;
-  const response = await fetch('http://localhost:8080/events/' + eventId, {
-    method: request.method,
-  });
-
-  if (!response.ok) {
-    throw json(
-      { message: 'Could not delete event.' },
-      {
-        status: 500,
-      }
-    );
-  }
-  return redirect('/events');
-
-}
