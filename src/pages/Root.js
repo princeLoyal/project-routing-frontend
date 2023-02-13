9c2ea58cedd01621f0eb44a59e9e272f77cef767
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-13 21:35:10 +0100

    Update Root.js

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,19 +1,6 @@
-import { Outlet, useNavigation } from 'react-router-dom';
-
-import MainNavigation from '../components/MainNavigation';
-
 function RootLayout() {
-  // const navigation = useNavigation();
-
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+ return (
+    <div>Hello</div>
+);
 }
-
 export default RootLayout;
