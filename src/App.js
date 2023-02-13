cbca6ed2e0df13af331df387bfccc6f9747bd225
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-14 00:14:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import EventDetailPage, {
 } from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import { action as manipulateEventAction } from './components/EventForm';
+import EditEventPage from './pages/EditEvent';
 
 const router = createBrowserRouter([
  {
@@ -35,18 +36,19 @@ const router = createBrowserRouter([
         {
          index: true,
          element: <EventDetailPage />,
-         //action: deleteEventAction,
+         action: deleteEventAction,
         },
         {
          path: 'edit',
          element: <EditEventPage />,
-         //action: manipulateEventAction,
+         action: manipulateEventAction,
         },
        ],
       },
       {
        path: 'new',
        element: <NewEventPage />,
+       action: manipulateEventAction,
       },
      ],
     },
