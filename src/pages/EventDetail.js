b044849f27a5bfc8861801ffd39e8e47820103b8
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-14 00:48:33 +0100

    Update EventDetail.js

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -35,12 +35,12 @@ async function loadEvent(id) {
   const response = await fetch('http://localhost:8080/events/' + id);
 
   if (!response.ok) {
-    throw json(
-      { message: 'Could not fetch details for selected event.' },
-      {
-        status: 500,
-      }
-    );
+    //throw json(
+    //  { message: 'Could not fetch details for selected event.' },
+     // {
+     //   status: 500,
+      //}
+   // );
   } else {
     const resData = await response.json();
     return resData.event;
