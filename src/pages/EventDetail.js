3f25614c6ee40fcec789796a9235a539e44ae907
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-14 22:15:14 +0100

    Update EventDetail.js

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -37,8 +37,7 @@ async function loadEvent(id) {
   const respData = await resp.json();
   for(const key in respData){
     if(respData[key].id === id){
-      //url = 'https://react-routing-eb51c-default-rtdb.firebaseio.com/events/' +key+ '.json':
-   url = 'sjhd';
+      url = `https://react-routing-eb51c-default-rtdb.firebaseio.com/events/${key}.json`:
 alert(url)
     }
   }
