9310db647b62488c4ec1fd972d83a229d0c906d5
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-14 11:44:39 +0100

    Update EditEvent.js

--- src/pages/EditEvent.js
+++ src/pages/EditEvent.js
@@ -4,7 +4,6 @@ import EventForm from '../components/EventForm';
 
 function EditEventPage(){
 const data = useRouteLoaderData('event-detail');
-alert(data.event)
 return(
 <EventForm event={data.event} method='patch'/>
 );
