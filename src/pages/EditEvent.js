00a60de49c1bd327b219122d3965de393fa79038
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-14 07:40:07 +0100

    Update EditEvent.js

--- src/pages/EditEvent.js
+++ src/pages/EditEvent.js
@@ -3,7 +3,7 @@ import { useRouteLoaderData } from 'react-router-dom';
 import EventForm from '../components/EventForm';
 
 function EditEventPage(){
-const data = useRouteLoaderData('event-detail');
+//const data = useRouteLoaderData('event-detail');
 return(
 <EventForm >
 );
