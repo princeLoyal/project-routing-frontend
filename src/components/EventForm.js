797b7eaf6817acf8778a844c5e5f526343ae7ba3
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-14 12:23:33 +0100

    Update EventForm.js

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -83,3 +83,40 @@ function EventForm({ method, event }) {
 
 export default EventForm;
 
+export async function action({ request, params }) {
+  const method = request.method;
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description'),
+  };
+
+  let url = 'https://react-routing-eb51c-default-rtdb.firebaseio.com/events.json';
+
+  if (method === 'patch') {
+alert('hdhdh')
+    const eventId = params.eventId;
+    url = 'https://react-routing-eb51c-default-rtdb.firebaseio.com/events' +eventId+ '.json';
+  }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: 'Could not save event.' }, { status: 500 });
+  }
+
+  return redirect('/events');
+}
