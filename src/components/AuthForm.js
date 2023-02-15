fe2ce0ead0008db40850be2086ee39a2cdcc967b
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-15 14:52:45 +0100

    Create AuthForm.js

---
+++ src/components/AuthForm.js
@@ -0,0 +1,33 @@
+import { Form, Link, useSearchParams, useActionData } from 'react-router-dom';
+
+import classes from './AuthForm.module.css';
+
+function AuthForm() {
+  const [searchParam] = useSearchParams();
+  const isLogin = searchParam.get('mode') === 'login';
+  const data = useActionData();
+  return (
+    <>
+      <Form method="post" className={classes.form}>
+        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        { data && data.errors && <ul>
+          {Object.values(data.errors).map(err => <li key={err}>{err}</li>)}
+          </ul>}
+        <p>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" required />
+        </p>
+        <p>
+          <label htmlFor="image">Password</label>
+          <input id="password" type="password" name="password" required />
+        </p>
+        <div className={classes.actions}>
+          <Link to={`?mode=${isLogin? 'signUp': 'login'}`}>
+            {isLogin ? 'Create new user' : 'Login'}
+          </Link>
+          <button>Save</button>
+        </div>
+      </Form>
+    </>
+  );
+}
