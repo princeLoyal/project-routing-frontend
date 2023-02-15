56a9a6723bce681014c2effb8ac1f991c08cbf29
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-15 19:51:27 +0100

    Update Authentication.js

--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -2,7 +2,7 @@ import AuthForm from '../components/AuthForm';
 import { json, redirect } from 'react-router-dom';
 
 function AuthenticationPage() {
-  return <div><AuthForm /></div>;
+  return <div>jdhdj</div>;
 }
 
 export default AuthenticationPage;
