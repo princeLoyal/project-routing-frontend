d27370e6c79597afc9768231d2ddac448641e1b1
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-15 15:17:09 +0100

    Create Auth.js

---
+++ src/pages/Auth.js
@@ -0,0 +1,42 @@
+import AuthForm from '../components/AuthForm';
+import { json, redirect } from 'react-router-dom';
+
+function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export default AuthenticationPage;
+
+export const action = async({ request }) => {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+
+  if(mode !== 'login' && mode !== 'signUp'){
+    throw json({message: 'Unsupported mode'}, { status: 422});
+  };
+
+  const response = await fetch('https://react-routing-eb51c-default-rtdb.firebaseio.com/user.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(authData)
+  });
+
+  if(response.status === 422 || response.status === 401){
+    return response;
+  };
+
+  if(!response.ok){
+    throw json({message: 'Could not autenticate user'}, { status: 500});
+  };
+
+  return redirect('/');
+
+};
