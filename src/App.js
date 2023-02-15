bc7e9318a1891116ce3babf0ecbd9971e4bd80c0
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-15 19:50:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import EventDetailPage, {
 import NewEventPage from './pages/NewEvent';
 import { action as manipulateEventAction } from './components/EventForm';
 import EditEventPage from './pages/EditEvent';
-import AuthenticationPage from './pages/Authentication';
+import AuthenticationPage  from './pages/Authentication';
 
 const router = createBrowserRouter([
  {
