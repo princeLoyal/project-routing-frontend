a54ee5e1568c4c0f3ccf30b46716a8cdb8c8dd65
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-16 11:15:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -56,8 +56,8 @@ const router = createBrowserRouter([
      ],
     },
     {
-     path: 'auth', 
-     element: <AuthenticationPage />,
+     path: {token? 'logout' : 'login' }, 
+     element: {token? <Logout /> : <AuthenticationPage /> },
      action: authAction, 
     },
   ],
