cdae00a88a3ff75a0dc32904266ab748e3813108
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-02-17 11:27:50 +0100

    Update MainNavigation.js

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,10 +1,11 @@
-import { NavLink, useRouteLoaderData } from 'react-router-dom';
+import { Form, NavLink, useRouteLoaderData } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 import NewsletterSignup from './NewsletterSignup';
 
 function MainNavigation() {
   const token = useRouteLoaderData('root');
+
   return (
     <header className={classes.header}>
       <nav>
@@ -30,16 +31,35 @@ function MainNavigation() {
               Events
             </NavLink>
           </li>
-          { token? && (<li>
+          <li>
             <NavLink
-              to='/login?mode=login'
+              to="/newsletter"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
             >
-              Login
+              Newsletter
             </NavLink>
-          </li> )}
+          </li>
+          {!token && (
+            <li>
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Authentication
+              </NavLink>
+            </li>
+          )}
+          {token && (
+            <li>
+              <Form action="/logout" method="post">
+                <button>Logout</button>
+              </Form>
+            </li>
+          )}
         </ul>
       </nav>
       <NewsletterSignup />
